16c3e7d67b4c176c2088eaed9c4eab290fc460b0
ioBroker Adapter Creator <noreply@iobroker.net>
2022-01-31 15:09:29 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,101 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"lightcontrol adapter settings": {
+		"en": "Adapter settings for lightcontrol",
+		"de": "Adaptereinstellungen für lightcontrol",
+		"ru": "Настройки адаптера для lightcontrol",
+		"pt": "Configurações do adaptador para lightcontrol",
+		"nl": "Adapterinstellingen voor lightcontrol",
+		"fr": "Paramètres d'adaptateur pour lightcontrol",
+		"it": "Impostazioni dell'adattatore per lightcontrol",
+		"es": "Ajustes del adaptador para lightcontrol",
+		"pl": "Ustawienia adaptera dla lightcontrol",
+		"zh-cn": "lightcontrol的适配器设置"
+	},
+	"LuxSensor": {
+		"en": "Lux Sensor",
+		"de": "Lux-Sensor",
+		"ru": "Датчик люкс",
+		"pt": "Sensor Lux",
+		"nl": "Lux-sensor",
+		"fr": "Capteur Lux",
+		"it": "Sensore Lux",
+		"es": "Sensor de luz",
+		"pl": "Czujnik luksów",
+		"zh-cn": "照度传感器"
+	},
+	"IsPresenceDp": {
+		"en": "Object for presence",
+		"de": "Objekt für Präsenz",
+		"ru": "Объект присутствия",
+		"pt": "Objeto para presença",
+		"nl": "Object voor aanwezigheid",
+		"fr": "Objet de présence",
+		"it": "Oggetto per presenza",
+		"es": "Objeto de presencia",
+		"pl": "Obiekt dla obecności",
+		"zh-cn": "存在的对象"
+	},
+	"PresenceCountDp": {
+		"en": "Object for presence counter",
+		"de": "Objekt für Anwesenheitszähler",
+		"ru": "Объект для счетчика присутствия",
+		"pt": "Objeto para contador de presença",
+		"nl": "Object voor aanwezigheidsteller",
+		"fr": "Objet pour compteur de présence",
+		"it": "Oggetto per contatore presenze",
+		"es": "Objeto para contador de presencia",
+		"pl": "Obiekt licznika obecności",
+		"zh-cn": "存在计数器的对象"
+	},
+	"RampSteps": {
+		"en": "Ramp steps for dimming",
+		"de": "Rampenstufen zum Dimmen",
+		"ru": "Ступени рампы для диммирования",
+		"pt": "Degraus de rampa para escurecimento",
+		"nl": "Hellingstappen voor dimmen",
+		"fr": "Marches de rampe pour la gradation",
+		"it": "Gradini di rampa per l'oscuramento",
+		"es": "Pasos de rampa para atenuación",
+		"pl": "Stopnie rampy do ściemniania",
+		"zh-cn": "用于调光的斜坡步骤"
+	},
+	"minCt": {
+		"en": "Colortemperature minimum",
+		"de": "Farbtemperatur Minimum",
+		"ru": "Минимум цветовой температуры",
+		"pt": "Temperatura de cor mínima",
+		"nl": "Minimaal kleurtemperatuur",
+		"fr": "Température de couleur minimum",
+		"it": "Temperatura colore minima",
+		"es": "Temperatura de color mínima",
+		"pl": "Minimalna temperatura barwowa",
+		"zh-cn": "最低色温"
+	},
+	"maxCt": {
+		"en": "Colortemperature maximum",
+		"de": "Maximale Farbtemperatur",
+		"ru": "Максимальная цветовая температура",
+		"pt": "Temperatura de cor máxima",
+		"nl": "Kleurtemperatuur maximum",
+		"fr": "Température de couleur maximale",
+		"it": "Temperatura colore massima",
+		"es": "Temperatura de color máxima",
+		"pl": "Maksymalna temperatura barwowa",
+		"zh-cn": "色温最大值"
+	},
+	"minBri": {
+		"en": "Brightness minimum for Adaptive Brightness",
+		"de": "Helligkeitsminimum für Adaptive Helligkeit",
+		"ru": "Минимум яркости для адаптивной яркости",
+		"pt": "Brilho mínimo para brilho adaptável",
+		"nl": "Minimale helderheid voor adaptieve helderheid",
+		"fr": "Luminosité minimale pour la luminosité adaptative",
+		"it": "Luminosità minima per Luminosità adattiva",
+		"es": "Brillo mínimo para Brillo adaptable",
+		"pl": "Minimalna jasność dla jasności adaptacyjnej",
+		"zh-cn": "自适应亮度的最低亮度"
+	}
+};
